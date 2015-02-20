57424f31ce401bab34615a2b0b86d42b06742288
Stephen Sowole <tunsow@hotmail.com>
2015-02-20 03:30:56 +0000

    Completed Dots Unity Project
    Game features for the project:
    * Populate the board with dots
    * Connect like coloured dots
    * Replenish the board with dots
    * The Square Mechanic: When a square is created, all like coloured dots
    are removed from the board.
    * Dots dropping animation.
    * Unlink dots that have been previously connected.

---
+++ StephenSowoleDotsRemake/DotsClassicRemakeStephenSowole/Assets/GameSceneController.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameSceneController : MonoBehaviour {
+
+	public int dotsSelected = 0;
+
+	// this variable is for when a mouse is being clicked/dragged
+	public bool isScreenBeingTouched = false;
+
+	// number of dots on board (has to be a square and even number otherwise will not work correctly)
+	private static int numberOfDots = 36;
+	private int numberOfRows;
+
+	// create dot sprite that all cloned dots will use
+	public WhiteDotObject dot;
+	private List<WhiteDotObject> dotList;
+
+	// previous dot
+	public WhiteDotObject previousDot = null;
+
+	// Use this for initialization
+	void Start () {
+
+		dotList = new List<WhiteDotObject>();
+
+		// get the number of rows/columns
+		numberOfRows = (int) Mathf.Sqrt (numberOfDots);
+
+		// create dots
+		spawnDots();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		// once mouse is not being clicked, unselected any selected dots
+		if (isScreenBeingTouched == false) {
+
+			unselectAndRemoveDots ();
+		}
+	}
+
+	void unselectAndRemoveDots() {
+
+		// iterate through dot list
+		for (int i = 0; i < numberOfDots; i++) {
+
+			WhiteDotObject dotObject = dotList[i];
+
+			// only execute if a dot is actually selected
+			if (dotObject.isSelected) {
+
+				// check if a square has been created first
+				if (dotObject.squareCompleted) {
+
+					dotObject.squareCompleted = false;
+
+					// if square has been created then select all dots that have the same colour as the selected squares
+					foreach (WhiteDotObject selectedDotObject in dotList) {
+
+						if (selectedDotObject.GetComponent<SpriteRenderer>().color == previousDot.GetComponent<SpriteRenderer>().color) {
+
+							selectedDotObject.isSelected = true;
+						}
+					}
+
+					previousDot = null;
+
+					// recursion to remove dots once they have been selected
+					unselectAndRemoveDots();
+				
+				// make sure that at least 2 dots have been selected
+				} else if (dotsSelected > 1) {
+
+					// the whole point of startPos is to get the starting position within the array so that ONLY VERTICAL dots around the
+					// selected dot are moved down. This is to save time so that other dots are not checked that do not need to be moved down
+					int startPos = -numberOfRows/2;
+
+					startPos = (int)dotObject.transform.position.x - startPos;
+
+					startPos *= numberOfRows;
+
+					// this for loop will check all dots above and below the selected dot
+					for (int j = startPos; j < (startPos+numberOfRows); j++) {
+
+						// any dots that are above the selected dot are moved down
+						if (dotList[j].transform.position.y > dotObject.transform.position.y) {
+
+							// first, make sure dot is in position to be moved down (for animation purposes)
+							dotList[j].transform.position = new Vector2(dotList[j].transform.position.x, dotList[j].clampYPos);
+
+							// set the physics to happen automatically
+							dotList[j].rigidbody2D.isKinematic = false;
+
+							// decrement the clamp Y posision so that the dot only falls one position downwards in the y axis
+							dotList[j].clampYPos--;
+						}
+					}
+
+					// destroy original selected dot
+					Destroy(dotObject.gameObject);
+
+					// remove dot from list
+					dotList.RemoveAt(i);
+
+					// create new dot at the top of the dot grid
+					Vector2 spawnPosition = new Vector2 (dotObject.transform.position.x, numberOfRows/2);
+
+					// insert new initialized dot in old dots place
+					dotList.Insert(i, Instantiate(dot, spawnPosition, Quaternion.identity) as WhiteDotObject);
+
+					// allow dot to drop
+					dotList[i].rigidbody2D.isKinematic = false;
+
+					// set the clamp position to be one below starting position (animation purposes)
+					dotList[i].clampYPos = spawnPosition.y - 1;
+
+					// set the minimum Y position that the dot can drop to
+					dotList[i].minimumYPos = -numberOfRows/2;
+				}
+
+				dotObject.isSelected = false;
+			}
+		}
+
+		previousDot = null;
+		dotsSelected = 0;
+	}
+
+	private void spawnDots() {
+
+		Vector2 spawnPosition;
+
+		int count = 0;
+
+		// create an x by x grid of dots
+		for (int row = -numberOfRows/2; row < numberOfRows/2; row++) {
+
+			for (int column = -numberOfRows/2; column < numberOfRows/2; column++) {
+
+				spawnPosition = new Vector2 (row,column);
+
+				// create a clone of the dot object
+				dotList.Add(Instantiate(dot, spawnPosition, Quaternion.identity) as WhiteDotObject);
+
+				// clamp position set from the start so that the y position is constantly remembered
+				dotList[count].clampYPos = column;
+
+				// set the lowest point that the dot can drop to.
+				dotList[count++].minimumYPos = -numberOfRows/2;
+			}
+		}
+	}
+}
---
+++ StephenSowoleDotsRemake/DotsClassicRemakeStephenSowole/Assets/WhiteDotObject.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhiteDotObject : MonoBehaviour {
+
+	static private float connectedLineWidth = 0.15f;
+	public WhiteDotObject previousDot = null;
+
+	// 'when selected' variables
+	public bool isSelected = false;
+	public bool squareCompleted = false;
+
+	// clamping the dots into position (dots dropping animation)
+	public float clampYPos = 0;
+	public float minimumYPos = 0;
+
+	LineRenderer lineRenderer;
+
+	// gameSceneController access
+	GameSceneController gameLoopScript;
+	GameObject gameLoop;
+
+	// Use this for initialization
+	void Start () {
+
+		// set the colour of the white dot once the dot is initialized
+		setDotColour();
+
+		lineRenderer = (LineRenderer) gameObject.AddComponent("LineRenderer");
+		lineRenderer.material = new Material (Shader.Find("Self-Illumin/Diffuse"));
+		lineRenderer.material.color = GetComponent<SpriteRenderer> ().color;
+		lineRenderer.SetWidth (connectedLineWidth, connectedLineWidth);
+
+		gameLoop = GameObject.Find("MainGameLoop");
+		gameLoopScript = gameLoop.GetComponent<GameSceneController> ();   
+    }
+
+	private void OnDestroy() {
+
+		Destroy(lineRenderer.material);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// once the dot stops moving after bouncing, clamp the dot into position
+		// second conditional is for the dots at the bottom, so that they dont fall through the bottom of the screen
+		if ((transform.position.y < clampYPos && rigidbody2D.velocity == Vector2.zero && !rigidbody2D.isKinematic) ||
+		    (transform.position.y < minimumYPos)) {
+
+			// disable automatic physics
+			rigidbody2D.isKinematic = true;
+
+			// clamp dots to nearest int position
+			transform.position = new Vector2 (Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+
+			// then move dot into clamped position
+			transform.position = new Vector2 (Mathf.Round(transform.position.x), clampYPos);
+		}
+
+		if (gameLoopScript.previousDot == this && !gameLoopScript.previousDot.squareCompleted) {
+
+			drawConnectingLine();
+        }
+	}
+
+	void OnMouseEnter() {
+
+		// if dot has been touched and is ready to connect to another dot
+		if (gameLoopScript.isScreenBeingTouched) {
+
+			if (!gameLoopScript.previousDot) {
+			
+				gameLoopScript.previousDot = this;
+				gameLoopScript.dotsSelected++;
+				isSelected = true;
+
+			} else if (!gameLoopScript.previousDot.squareCompleted) {
+			
+				if (!isSelected) {
+
+					// the purpose of x and y is to get the difference in between two connected dots in terms of coordinates
+					double x = Mathf.Abs(gameLoopScript.previousDot.transform.position.x - transform.position.x);
+					double y = Mathf.Abs(gameLoopScript.previousDot.transform.position.y - transform.position.y);
+					
+					// if total difference is either 1 or -1 then it means the dots are horizontally or vertically connected
+					// if more/less then the dots are diagonal which is incorrect. if x+y = 0 then it means that its the same dot
+
+					if ((x + y) == 1 || (x + y) == -1) {
+						
+						// then check if both dots are the same colour
+						if (gameLoopScript.previousDot.GetComponent<SpriteRenderer>().color == GetComponent<SpriteRenderer>().color) {
+							
+							// if both conditions are satisfied then select dot
+							isSelected = true;
+							gameLoopScript.previousDot.drawConnectedLine(transform.position);
+							this.previousDot = gameLoopScript.previousDot;
+							gameLoopScript.previousDot = this;
+							gameLoopScript.dotsSelected++;
+							lineRenderer.SetVertexCount(2); 
+						}
+					}
+
+				} else if (gameLoopScript.previousDot.previousDot == this) {
+
+					gameLoopScript.previousDot.isSelected = false;
+					gameLoopScript.previousDot.removeConnectedLine();
+					gameLoopScript.previousDot.previousDot = null;
+					gameLoopScript.previousDot = this;
+					gameLoopScript.dotsSelected--;
+
+				} else if (gameLoopScript.previousDot != this && gameLoopScript.previousDot.previousDot != this) {
+
+					double x = Mathf.Abs(gameLoopScript.previousDot.transform.position.x - transform.position.x);
+					double y = Mathf.Abs(gameLoopScript.previousDot.transform.position.y - transform.position.y);
+
+					if ((x + y) == 1 || (x + y) == -1) {
+
+						squareCompleted = true;
+						gameLoopScript.previousDot.drawConnectedLine(this.transform.position);
+						gameLoopScript.previousDot = this;
+						isSelected = true;
+					}
+				}
+			}
+		}
+	}
+
+	void OnMouseDown() {
+
+		lineRenderer.SetVertexCount(2); 
+		gameLoopScript.isScreenBeingTouched = true;
+		OnMouseEnter();
+	}
+
+	public void removeConnectedLine() {
+	
+		lineRenderer.SetVertexCount(0);
+	}
+
+	public void drawConnectedLine(Vector2 endPos) {
+
+		lineRenderer.SetPosition (0, transform.position);
+		lineRenderer.SetPosition (1, endPos);
+    }
+
+	private void drawConnectingLine() {
+
+		lineRenderer.SetPosition (0, transform.position);
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		lineRenderer.SetPosition (1, new Vector2(mousePos.x, mousePos.y));
+	}
+
+	void OnMouseUp() {
+
+		lineRenderer.SetVertexCount(0); 
+		gameLoopScript.isScreenBeingTouched = false;
+	}
+
+	private void setDotColour() {
+
+		// sets a random dot colour once the dot has been initialized
+
+		switch(Random.Range(0, 5)) {
+
+		case 0:
+			// Purple
+			GetComponent<SpriteRenderer>().color = new Color(0.729f, 0.333f, 0.827f, 1.000f);
+			break;
+
+		case 1:
+			// Blue
+			GetComponent<SpriteRenderer>().color = new Color(0.118f, 0.565f, 1.000f, 1.000f);
+			break;
+		
+		case 2:
+			// Red
+			GetComponent<SpriteRenderer>().color = new Color(1.000f, 0.388f, 0.278f, 1.000f);
+			break;
+
+		case 3:
+			// Green
+			GetComponent<SpriteRenderer>().color = new Color(0.565f, 0.933f, 0.565f, 1.000f);
+			break;
+
+		default:
+			// Yellow
+			GetComponent<SpriteRenderer>().color = new Color(1.000f, 0.843f, 0.000f, 1.000f);
+			break;
+		}
+	}
+}
